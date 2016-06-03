85d383aeea9170c5786954b8ea71fa06c1923e71
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 13:42:24 -0400

    added require() for admin login to main admin index page

--- dsh/index.php
+++ dsh/index.php
@@ -3,6 +3,7 @@
 	//import template to check if admin is logged in
 	
 	//if they aren't, include the login form with all its glorious headers
+	require "adminlogin.php";
 	
 	//if they are, welcome them, echo headers for full admin page
 	//Then echo the three buttons and information about last login date and IP
