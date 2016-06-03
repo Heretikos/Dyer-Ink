c34f8c2ea32ae38455d97bc27de290af90281434
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 16:01:49 -0400

    Decided to take a different approach instead of commenting everything out

--- dsh/index.php
+++ dsh/index.php
@@ -3,15 +3,15 @@
 	//import template to check if admin is logged in
 	require "adminCheck.php";
 	
-	//if($adminLoggedIn) {
+	if($adminLoggedIn) {
 		//if they are, welcome them, echo headers for full admin page
 		echo "admin is logged in!<br />";
 		
 		//Then echo the three buttons and information about last login date and IP
 		
-	//} else {
+	} else {
 		//if they aren't, include the login form with all its glorious headers
 		require "adminlogin.php";
-	//}
+	}
 	
 ?>
