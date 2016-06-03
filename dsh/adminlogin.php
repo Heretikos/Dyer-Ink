0f1d6691fbe0004f9960f8e4b21783a0f65030cf
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 15:25:23 -0400

    got all the database and admin login code working

--- dsh/adminlogin.php
+++ dsh/adminlogin.php
@@ -34,6 +34,13 @@
 			.adminLogin input[type=submit] {
 				padding:10px 32px;
 			}
+			.error {
+				color:red;
+				font-size:1.45em;
+				text-align:center;
+				margin:auto;
+				font-weight:bold;
+			}
 		</style>
 	</head>
 	<body>
@@ -48,49 +55,75 @@
 		$user = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
 		$pass = filter_var($_POST['pass'], FILTER_SANITIZE_STRING);
 		
-		
+		/* I COMMENTED THIS SHIT OUT FOR LOCAL TESTING
 		
 		//Then, check against values in database 
 		require "adminmysqlcreds.php";
 		
 		// Create connection
-		$conn = new mysqli($servername, $username, $password,$dbname);
+		$conn = new mysqli($servername, $dbusername, $dbpassword,$dbname);
 	
 		// Check connection
 		if ($conn->connect_error) {
 				die("Connection failed: " . $conn->connect_error);
 		} 
-		echo "Connected successfully";
 		
+		//query database
+		$sql = "SELECT * FROM users WHERE username = '$user' AND password = '$pass'";
+		$result = $conn->query($sql);
+	
+		//check if they're in the database
+		if ($result->num_rows > 0) {
+			echo "username and password matched";
+			//If they match, set cookie, 
+			
+			
+			//echo login successful, 
+			
+			
+			//and some inline javascript to reload the page. 
+			
+			
+			//Then email login details (time, IP address) to admin@dyerink.com
+			$msg = "From IP: " . $_SERVER['REMOTE_ADDR'] . "\nWith username: $user and password $pass";
+			
+			mail("admin@dyerink.com","Admin login by $user",$msg);
+			
+		}
+		else {
+			
+			//If they don't match, log IP address, email login attempt details (including attempted
+			//password) to admin@dyerink. 
+			
+			$msg = "From IP: " . $_SERVER['REMOTE_ADDR'] . "\nWith username: $user and password $pass";
+			
+			mail("admin@dyerink.com","Failed Login Attempt",$msg);
+			
+			
+			
+			//Then echo that the password or username was incorrect
+			
+			echo '<form class="adminLogin" method="post" action="index.php">';
+			echo '<div class="error">Incorrect username or password</div>';
+			echo '<input type="text" placeholder="Username" name="username" /> <br />';
+			echo '<input type="password" placeholder="Password" name="pass" /> <br />';
+			echo '<input type="submit" value="Login" />';
+			echo '</form>';
+			
+		}
 		
-		//If they match, set cookie, echo login successful, and some inline javascript to
-		//reload the page. Then email login details (time, IP address) to admin@dyerink.com
+		THE ABOVE CODE IS COMMENTED OUT FOR TESTING ON A SERVER SANS MYSQL*/
+		
+	} else {
+		//If not, echo a basic form for login
+		echo '<form class="adminLogin" method="post" action="index.php">';
+		echo '<input type="text" placeholder="Username" name="username" /> <br />';
+		echo '<input type="password" placeholder="Password" name="pass" /> <br />';
+		echo '<input type="submit" value="Login" />';
+		echo '</form>';
 	}
 	
-	
-	
-	
-	
-	//If they don't match, log IP address, email login attempt details (including attempted
-	//password) to admin@dyerink. Then echo that the password or username was incorrect
-	
-	
-	
-	
-	//If not, echo a basic form for login
-	
-?>
-
-<form class="adminLogin" method="post" action="index.php">
-	<input type="text" placeholder="Username" name="username" /> <br />
-	<input type="password" placeholder="Password" name="pass" /> <br />
-	<input type="submit" value="Login" />
-</form>
-
-<?php
-
 	//closing tags
-
 ?>
 		
 	</body>
