8f1c7fb722b358add682fa89e5e8df440b917df8
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 15:38:30 -0400

    Added admin check to dsh index page

--- dsh/index.php
+++ dsh/index.php
@@ -1,14 +1,17 @@
 <?php
 
 	//import template to check if admin is logged in
+	require "adminCheck.php";
 	
-	
-	//if they aren't, include the login form with all its glorious headers
-	require "adminlogin.php";
-	
-	//if they are, welcome them, echo headers for full admin page
-	//Then echo the three buttons and information about last login date and IP
-	
-	//echo closing tags
+	if($adminLoggedIn) {
+		//if they are, welcome them, echo headers for full admin page
+		echo "admin is logged in!<br />";
+		
+		//Then echo the three buttons and information about last login date and IP
+		
+	} else {
+		//if they aren't, include the login form with all its glorious headers
+		require "adminlogin.php";
+	}
 	
 ?>
