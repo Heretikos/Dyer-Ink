8ce6eac0f10e2dae87c3aec2f743d9bef1994847
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 01:38:47 -0400

    Wrote pseudocode for additem.php

--- dsh/additem.php
+++ dsh/additem.php
@@ -1,6 +1,14 @@
 <?php
+	//include PHP template with concept of InventoryItem
+	//require headers and shit
 
-	//if $_GET parameters are set, load this page up with the appropriate information
-	//for that itemID
+	//if $_GET parameters are set, instantiate a PHP object out of those parameters
+	//and then call its methods for rendering the form populated (might just be a particular 
+	//include())
+	
+	//if $_POST parameters are set, instantiate a PHP object out of those post parameters 
+	//and populate that object, then call its methods to push that info into database,
+	//then echo "item added" or something
 
+	//include fooders and shit
 ?>
