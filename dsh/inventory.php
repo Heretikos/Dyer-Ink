0fade2e964ca879c58bd730a1738ef3a9df1591f
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 01:45:49 -0400

    Finalized inventory pseudocode

--- dsh/inventory.php
+++ dsh/inventory.php
@@ -8,7 +8,8 @@
 	
 	//connect to database, authenticate
 	
-	//get count of items
+	//get count of items/number of entries in inventory table
+	
 	//if it's zero, echo "there's nothing in your inventory, click above to add an item!"
 	//If it's above zero, do an SQL query to fetch the contents
 	//Then, spit that shit out with a for() loop
