beb3f34752ab129e105188e431401dacb83c1263
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-03 01:18:53 -0400

    Wrote pseudocode for admin login

---
+++ dsh/adminlogin.php
@@ -0,0 +1,26 @@
+<?php
+
+	//echo admin login stylesheet, headers, title
+	
+	//Check if $_POST variables for login form are set
+	
+	
+	
+	//If so, sanitize inputs.
+	
+	//Then, check against values in database 
+	
+	//If they match, set cookie, echo login successful, and some inline javascript to
+	//reload the page. Then email login details (time, IP address) to admin@dyerink.com
+	
+	//If they don't match, log IP address, email login attempt details (including attempted
+	//password) to admin@dyerink. Then echo that the password or username was incorrect
+	
+	
+	
+	
+	//If not, echo a basic form for login
+
+
+
+?>
