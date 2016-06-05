0955816204e979f4abaaa9ff98fce2ae2d0dda08
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-05 18:06:28 -0400

    More tweaking of styling

--- index.php
+++ index.php
@@ -27,13 +27,13 @@
 				<a href="http://youtube.com/dyerink">Videos</a>
 			</nav>
 			<aside id="social">
-				<img class="socialButton" src="https://cdn3.iconfinder.com/data/icons/free-social-icons/67/facebook_circle_color-128.png" width="70" /><br />
+				<img class="socialButton" src="https://cdn1.iconfinder.com/data/icons/logotypes/32/circle-google-plus-128.png" width="70" /><br />
+				<img class="socialButton" src="http://icons.iconarchive.com/icons/sicons/basic-round-social/512/facebook-icon.png" width="70" /><br />
 				<img class="socialButton" src="http://icons.iconarchive.com/icons/sicons/basic-round-social/512/twitter-icon.png" width="70" /><br />
-				<img class="socialButton" src="https://cdn0.iconfinder.com/data/icons/Pinterest/128/Pinterest_Favicon.png" width="70" /><br />
-				<img class="socialButton" src="" width="70" />
+				<img class="socialButton" src="https://cdn0.iconfinder.com/data/icons/Pinterest/128/Pinterest_Favicon.png" width="70" />
 			</aside>
 			<aside id="sideNav">
-				<a href="artist.html"><img width="150" class="sidebarImg" src="http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png" /><br />Artist</a><br />
+				<a href="artist.html"><img width="150" class="sidebarImg" src="theartist.jpg" style="overflow:hidden;clip:rect(0px,0px,150px,150px)" /><br />Artist</a><br />
 				<a href="art.html"><img width="150" class="sidebarImg" src="http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png" /><br />Art</a><br />
 				<a href="sharks.html"><img width="150" class="sidebarImg" src="jaw.jpg" /><br />Sharks</a>
 			</aside>
