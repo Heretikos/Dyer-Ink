d4385fef025a90e3b8372bebe7dcc6038fa4a327
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-05 16:00:08 -0400

    Created unpublish page

--- dsh/unpublish.php
+++ dsh/unpublish.php
@@ -1,9 +1,34 @@
 <?php
 	
 	//Connect to mySQL database
+	require "adminmysqlcreds.php";
+	
+	$dbname = "tsubasag_DI_inventory";
+	
+	// Create connection
+	$conn = new mysqli($servername, $dbusername, $dbpassword,$dbname);
+
+	// Check connection
+	if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+	} 
+	
 	
 	//fetch $_GET parameter
+	$publishID = $_GET["itemID"];
 	
-	//unpublish the item corresponding to that ID
+	//publish the item corresponding to that ID
+	
+	$sql = "UPDATE inventory SET published='0' WHERE id=$publishID";
+	
+	
+	if ($conn->query($sql) === TRUE) {
+		echo "Record updated successfully";
+		echo "<script>window.location.href='index.php';</script>";
+	} else {
+		echo "Error updating record: " . $conn->error;
+	}
+	
+	$conn->close();
 	
 ?>
