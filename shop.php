086d2075a629150d6912d0a69eb0cc249f2e51ff
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-05 11:20:01 -0400

    Outlined most of base HTML for shop and imported fonts

--- shop.php
+++ shop.php
@@ -4,6 +4,34 @@
 		<link rel="stylesheet" href="style.css" />
 	</head>
 	<body>
-		
+		<div id="content">
+			<img id="logo" src="logo.png" />
+			<h2>Dyer Ink</h2>
+			<nav>
+				<a href="artist.html">Artist</a>
+				<a href="art.html">Art</a>
+				<a href="sharks.html">Sharks</a>
+				<a href="shop.php">Shop</a>
+				<a href="contact.php">Contact</a>
+				<a href="http://dyerink.com">Blog</a>
+				<a href="videos.html">Videos</a>
+			</nav>
+			<aside>
+				<img src="" />
+				<img src="" />
+				<img src="" />
+				<img src="" />
+			</aside>
+			<article>
+				<h1>The</h1>
+				<h1>Shark</h1>
+				<h1>Shop</h1>
+			</article>
+			<aside>
+				<a href="artist.html"><img class="sidebar" /><br />Artist</a>
+				<a href="art.html"><img class="sidebar" /><br />Art</a>
+				<a href="sharks.html"><img class="sidebar" /><br />Sharks</a>
+			</aside>
+		</div>
 	</body>
 </html>
