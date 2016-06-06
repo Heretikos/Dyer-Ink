e114f68dbbbdc3f8988dc4c8a169cc960363a7f5
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-05 21:43:47 -0400

    hooking up inventory to the database

--- dsh/inventory.php
+++ dsh/inventory.php
@@ -83,25 +83,48 @@
 	
 <?php
 	
+		
+		//Echo H1 that says inventory
+		echo "<h1>Inventory</h1>";
+		
+		//Echo addItem button (just a link to /addItem.php really)
+		echo "<a href='addItem.php'>Add Item</a>";
+		
+		//connect to database, authenticate
+		require "adminsqlcreds.php";
+		
+		$dbname = "tsubasag_DI_inventory";
+		
+		$conn = new mysqli($servername, $username, $password,$dbname);
 	
-	//Echo H1 that says inventory
-	echo "<h1>Inventory</h1>";
+		// Check connection
+		if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+		} 
+		
+		//get count of items/number of entries in inventory table
+		
+		$sql = "SELECT * FROM inventory";
+		$result = $conn->query($sql);
+		$itemCount = $result->num_rows;
 	
-	//Echo addItem button (just a link to /addItem.php really)
-	echo "<a href='addItem.php'>Add Item</a>";
-	
-	//connect to database, authenticate
-	
-	//get count of items/number of entries in inventory table
-	
-	//if it's zero, echo "there's nothing in your inventory, click above to add an item!"
-	//If it's above zero, do an SQL query to fetch the contents
-	//Then, spit that shit out with a for() loop
-		//div, image with class that makes it float left, h2 with item name, p with details
-		//div inside of that with icons and ahrefs for deleting, editing,
-		//publish/unpublishing that item (using $_GET parameters in the URLs) with class
-		//that makes it invisible on mobile
-	//echo closing tags
+		if ($itemCount > 0) {
+				//If it's above zero, do an SQL query then, spit that shit out with a for() loop
+				while($row = $result->fetch_assoc()) {
+					//div, image with class that makes it float left, h2 with item name, p with details
+					//div inside of that with icons and ahrefs for deleting, editing,
+					//publish/unpublishing that item (using $_GET parameters in the URLs) with class
+					//that makes it invisible on mobile
+					echo  "<br>" . $row["id"];
+					}
+		} else {
+				//if it's zero, echo "there's nothing in your inventory, click above to add an item!"
+				echo "There's nothing in your inventory, click above to add an item!";
+		}
+		$conn->close();
+		
+
 	}
+	//echo closing tags
 	include "adminFooter.php";	
 ?>
