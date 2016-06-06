c8bdb340ca28621eba2806d95465705e523790f5
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-05 21:24:45 -0400

    since generalizing navigation via template screwed up class assignment for the 'active page', I'm just making all navigation look the same

--- nav.php
+++ nav.php
@@ -4,7 +4,7 @@
 	<a href="art.php">Art</a>
 	<a href="sharks.php">Sharks</a>
 	<a href="shop.php">Shop</a>
-	<a href="contact.php" class="activePage">Contact</a>
+	<a href="contact.php">Contact</a>
 	<a href="http://dyerink.com/wp/">Blog</a>
 	<a href="videos.php">Videos</a>
 </nav>
