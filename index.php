84025b6fd87449052d88bda58eb46d21703ec29f
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-06 22:24:22 -0400

    realigned the headings and enter button on the landing page

--- index.php
+++ index.php
@@ -17,14 +17,14 @@
 		   <h1>Dyer Ink</h1>
 			<?php include "nav.php"; include "social.php" ?>
 			<aside id="sideNav">
-				<a href="artist.html"><img width="100" class="sidebarImg" src="theartist.jpg" style="overflow:hidden;clip:rect(0px,0px,150px,150px)" /><br />Artist</a><br />
-				<a href="art.html"><img width="100" class="sidebarImg" src="http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png" /><br />Art</a><br />
-				<a href="sharks.html"><img width="100" class="sidebarImg" src="jaw.jpg" /><br />Sharks</a>
+				<a href="artist.php"><img width="100" class="sidebarImg" src="theartist.jpg" style="overflow:hidden;clip:rect(0px,0px,150px,150px)" /><br />Artist</a><br />
+				<a href="art.php"><img width="100" class="sidebarImg" src="http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png" /><br />Art</a><br />
+				<a href="sharks.php"><img width="100" class="sidebarImg" src="jaw.jpg" /><br />Sharks</a>
 			</aside>
 			<article id="mainpage">
-				<h2>The</h2>
-				<h2>Shark</h2>
-				<h2>Shop</h2>
+				<h2 class="ssintro">The</h2>
+				<h2 class="ssintro">Shark</h2>
+				<h2 class="ssintro">Shop</h2>
 				<a href="" class="featuredLink">Clothing</a>
 				<a href="shopitem.php"><img id="enterButton" src="http://www.pngall.com/wp-content/uploads/2016/04/Enter-PNG-File.png" width="130" /></a>
 				<a href="" class="featuredLink">One of a Kind</a>
