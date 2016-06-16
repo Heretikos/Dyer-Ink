7fc37d4a5afff785a1953db54a6859ea604d7a4f
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-15 21:53:12 -0400

    fixed blog link in nav to prevent 404s

--- nav.php
+++ nav.php
@@ -5,6 +5,6 @@
 	<a href="sharks.php">Sharks</a>
 	<a href="shop.php">Shop</a>
 	<a href="contact.php">Contact</a>
-	<a href="http://dyerink.com/wp/">Blog</a>
+	<a href="http://dyerink.com/">Blog</a> <!-- CHANGE THIS TO LINK TO THE BLOG -->
 	<a href="videos.php">Videos</a>
 </nav>
