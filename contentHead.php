80a46694101e36a42b60e7aaec868b6a2065689c
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-17 00:35:22 -0400

    Testing access to and configuration of settings table

--- contentHead.php
+++ contentHead.php
@@ -4,3 +4,27 @@
 		<a href="index.php">
 			<img id="logo" src="http://cumbrianrun.co.uk/wp-content/uploads/2014/02/default-placeholder.png" width="150" />
 		</a>
+		
+		<?php 
+		require "mysqlcreds.php";
+		
+		$dbname = "test";
+		
+		// Create connection
+		$conn = new mysqli($servername, $dbusername, $dbpassword,$dbname);
+	
+		// Check connection
+		if ($conn->connect_error) {
+				die("Connection failed: " . $conn->connect_error);
+		} 
+		
+		$sql = "SELECT * FROM settings;";
+		$result = $conn->query($sql);
+		$itemCount = $result->num_rows;
+		
+		if ($row['bordersOn'] = 1) {
+			echo "bordersOn!";
+		}
+		
+		$conn->close();
+		?>
