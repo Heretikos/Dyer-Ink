ae42e634fb49a54d10c3e08d0663322a00afe10f
NilesBlack <NilesBlack@users.noreply.github.com>
2016-06-17 01:36:33 -0400

    trying yet another tweak - just pasting code I know works from the other page

--- contentHead.php
+++ contentHead.php
@@ -22,32 +22,35 @@
 		$result = $conn->query($sql);
 		$itemCount = $result->num_rows;
 		
-		echo $itemCount;
-		echo "has it updated7?";
-		while($row = $result->fetch_assoc()) {
-			echo $row['value'];
-			if ($row['value'] == 1) {
-			?>
-				
-				<style type="text/css">
-					.productListing, nav, #social {
-						border:1px solid white;
-					}
-				</style>
-				
-			<?php	
-			} else {
-			?>
-				
-				<style type="text/css">
-					.productListing, nav, #social {
-						border:0px solid transparent;
-					}
-				</style>
-				
-		
-			<?php
+		if ($itemCount > 0) {
+				//If it's above zero, do an SQL query then, spit that shit out with a for() loop
+			while($row = $result->fetch_assoc()) {
+				echo $row['value'];
+				if ($row['value'] == 1) {
+				?>
+					
+					<style type="text/css">
+						.productListing, nav, #social {
+							border:1px solid white;
+						}
+					</style>
+					
+				<?php	
+				} else {
+				?>
+					
+					<style type="text/css">
+						.productListing, nav, #social {
+							border:0px solid transparent;
+						}
+					</style>
+					
+			
+				<?php
+				}
 			}
+		} else {
+			echo "I have no freaking clue";
 		}
 		$conn->close();
 		?>
